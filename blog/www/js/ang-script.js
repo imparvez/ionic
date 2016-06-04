6d1bfc9a0880715a8cc898f6bc443e45d895a94c
Parvez shaikh <parvezshaikhj@gmail.com>
2016-06-04 20:02:41 +0530

    Blog added

---
+++ blog/www/js/ang-script.js
@@ -0,0 +1,31 @@
+angular.module('myBlog', ['ionic','ngRoute'])
+
+//Configure our routes
+.config(function($routeProvider){
+  $routeProvider
+  //route for the home page
+  .when('/',{
+    templateUrl:'index.html',
+    controller: 'myBlogListController'
+  })
+})
+
+.controller('myBlogListController',['$scope', '$log', function($scope, $log){
+  $scope.items = [
+    { 
+      id: 1,
+      blogName: 'blog-one',
+      album: 'Gotta Be Somebody', 
+      artist: 'Nickelback', 
+      image: 'http://lorempixel.com/200/200/' 
+    },
+    { 
+      id: 2,
+      blogName: 'blog-two',
+      album: 'Dark Horse', 
+      artist: 'Nickelback', 
+      image: 'http://lorempixel.com/200/200/' 
+    }
+  ];
+}])
+
