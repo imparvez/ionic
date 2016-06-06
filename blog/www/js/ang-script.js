dec0215978c40796d600e923c45941e4481cfef9
Parvez Shaikh <parvez.shaikh@bookmyshow.com>
2016-06-06 17:05:51 +0530

    worked on url parameters

--- blog/www/js/ang-script.js
+++ blog/www/js/ang-script.js
@@ -1,34 +1,32 @@
 angular.module('myBlog', ['ionic','ngRoute'])
 
 //Configure our routes
-.config(function($routeProvider){
+.config(['$routeProvider',function($routeProvider){
   $routeProvider
   //route for the home page
   .when('/',{
     templateUrl:'index.html',
     controller: 'myBlogListController'
   })
-})
+  .when('/route/:param',{
+    template:'Referred item is {{ctrl.id}}',
+    controller: ['$routeParams', function($routeParams) {
+        var self=this;
+        self.id = $routeParams.id;
+    }],
+    controllerAs: 'ctrl'
+  })
+}])
 
-.controller('myBlogListController', ['$scope', '$http', function($scope, $http){
+.controller('myBlogListController', ['$scope', '$http','$routeParams', function($scope, $http, $routeParams){
   $http.get('js/blogList.json').then(function(response){
     $scope.blogList = response.data;
   })
-  // $scope.items = [
-  //   { 
-  //     id: 1,
-  //     blogName: 'blog-one',
-  //     album: 'Gotta Be Somebody', 
-  //     artist: 'Nickelback', 
-  //     image: 'http://lorempixel.com/200/200/' 
-  //   },
-  //   { 
-  //     id: 2,
-  //     blogName: 'blog-two',
-  //     album: 'Dark Horse', 
-  //     artist: 'Nickelback', 
-  //     image: 'http://lorempixel.com/200/200/' 
-  //   }
-  // ];
+}])
+
+.controller('myBlogDetailsController', ['$scope', '$http','$routeParams', function($scope, $http, $routeParams){
+  $http.get('js/blogList.json').then(function(response){
+    $scope.blogDetails = response.data;
+  })
 }])
 
